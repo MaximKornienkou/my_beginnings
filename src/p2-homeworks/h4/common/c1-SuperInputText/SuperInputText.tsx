7766ffa878020ce6b9a97aae76372c505e5d722b
MaximKornienkou <82660318+MaximKornienkou@users.noreply.github.com>
2021-10-21 08:42:25 +0300

    first

--- src/p2-homeworks/h4/common/c1-SuperInputText/SuperInputText.tsx
+++ src/p2-homeworks/h4/common/c1-SuperInputText/SuperInputText.tsx
@@ -7,8 +7,8 @@ type DefaultInputPropsType = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElem
 // здесь мы говорим что у нашего инпута будут такие же пропсы как у обычного инпута
 // (чтоб не писать value: string, onChange: ...; они уже все описаны в DefaultInputPropsType)
 type SuperInputTextPropsType = DefaultInputPropsType & { // и + ещё пропсы которых нет в стандартном инпуте
-    onChangeText?: (value: string) => void
-    onEnter?: () => void
+    onChangeText?: (value: string) => void;
+    onEnter?: () => void;
     error?: string
     spanClassName?: string
 }
